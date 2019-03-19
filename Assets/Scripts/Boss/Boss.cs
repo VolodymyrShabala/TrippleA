abd8fb9c04b8338ae43a07ad9d4241acc827d096
VolodymyrShabala <33130565+VolodymyrShabala@users.noreply.github.com>
2019-03-19 16:33:19 +0100

    Alot of stuff
    Created boss, projectiles, animations

---
+++ Assets/Scripts/Boss/Boss.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Boss : MonoBehaviour, IDamageable {
+    protected Animator anim;
+
+    [SerializeField]
+    private int health = 10;
+
+
+    [SerializeField]
+    private Boss nextBoss;
+    [SerializeField]
+    private GameObject explosion;
+    [SerializeField]
+    private int numberOfExplosions = 25;
+    [SerializeField]
+    private float timeBetweenExplosions = 0.01f;
+
+    private bool isInvincible = false;
+
+    private void Awake() {
+        anim = GetComponent<Animator>();
+    }
+
+    protected void CanBeDamaged(bool canBeDamaged = true) {
+        isInvincible = canBeDamaged;
+    }
+
+    public void TakeDamage(int takenDamage = 1) {
+        if(isInvincible) {
+            return;
+        }
+        health -= takenDamage;
+        anim.SetBool("TookDamage", true);
+        if(health <= 0) {
+            Die();
+        }
+    }
+
+    public virtual void Die() {
+        StartCoroutine("SpawnExplosionEnum");
+    }
+
+    private void SpawnInNextBoss() {
+        anim.SetBool("Dead", true);
+        if(nextBoss) {
+            Instantiate(nextBoss, transform.position, Quaternion.identity);
+        }
+        Destroy(gameObject);
+    }
+
+    private void SpawnExplosionsOnDeath() {
+        StartCoroutine("SpawnExplosionEnum");
+    }
+
+    private IEnumerator SpawnExplosionEnum() {
+        for(int i = 0; i < numberOfExplosions; i++) {
+            Vector2 point = Random.insideUnitCircle;
+            Instantiate(explosion, point, Quaternion.identity);
+            new WaitForSeconds(timeBetweenExplosions);
+        }
+        SpawnInNextBoss();
+        return null;
+    }
+}
---
+++ Assets/Scripts/Boss/BossPhaseOne.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BossPhaseOne : Boss {
+    [SerializeField]
+    private Transform player;
+
+    [SerializeField]
+    private GameObject projectile;
+    [SerializeField]
+    private Transform shootFromTransform;
+    [SerializeField]
+    private int damage = 1;
+    [SerializeField]
+    private int straightShotsAmount = 1;
+    [SerializeField]
+    private float timeBeetweenShotsAmount = 0.3f;
+    [SerializeField]
+    private float attacksPerSecond = 10;
+    private float attackSpeed;
+    private float attackSpeedHolder;
+
+    private void Start() {
+        if(attacksPerSecond > 0) {
+            attackSpeed = 60 / attacksPerSecond;
+        }
+    }
+
+    private void Update() {
+        if(Time.time >= attackSpeedHolder) {
+            //anim.SetBool("Attacking", true);
+            //float r = Random.Range(0.0f, 1.0f);
+            //if(r <= 0.5) {
+                StartCoroutine("AttackStraight");
+            //} else {
+               // StartCoroutine("AttackStrafe");
+            //}
+            attackSpeedHolder = Time.time + attackSpeed;
+        }
+    }
+
+    private IEnumerator AttackStraight() {
+        for(int i = 0; i < straightShotsAmount; i++) {
+            GameObject go = Instantiate(projectile, shootFromTransform.position, Quaternion.identity);
+            BossPhaseOneProjectile pr = go.GetComponent<BossPhaseOneProjectile>();
+            pr.MoveTo(player.position);
+            pr.Damage = damage;
+            yield return new WaitForSeconds(timeBeetweenShotsAmount);
+        }
+        yield return null;
+    }
+
+    private IEnumerator AttackStrafe() {
+        return null;
+    }
+}
