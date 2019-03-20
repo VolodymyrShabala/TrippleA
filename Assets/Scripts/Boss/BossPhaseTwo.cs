a6e00459dca280a5e485fa47ad31ef6573b4aed3
VolodymyrShabala <33130565+VolodymyrShabala@users.noreply.github.com>
2019-03-20 16:51:50 +0100

    Second boss fixes

--- Assets/Scripts/Boss/BossPhaseTwo.cs
+++ Assets/Scripts/Boss/BossPhaseTwo.cs
@@ -1,8 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BossPhaseTwo : MonoBehaviour {
+public class BossPhaseTwo : MonoBehaviour, IDamageable {
     private Animator anim;
+    private SpriteRenderer spriteRenderer;
+    [SerializeField]
+    private Sprite damagedSprite;
+    private Sprite defaultSprite;
 
     [Header("HP")]
     [SerializeField]
@@ -12,21 +16,52 @@ public class BossPhaseTwo : MonoBehaviour {
     [SerializeField]
     private Boss nextBoss;
 
+    private bool invincible = true;
 
     private void Start() {
         anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        defaultSprite = spriteRenderer.sprite;
+        StartCoroutine("Spawn");
+    }
+
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            spriteRenderer.sprite = damagedSprite;
+        }
     }
 
     public void TakeDamage(int takenDamage = 1) {
-        health -= takenDamage;
+        if(invincible) {
+            return;
+        }
+
+        print("!");
         anim.SetTrigger("TakeDamage");
+        health -= takenDamage;
+        //StartCoroutine("TakenDamageVisualise");
         if(health <= 0) {
             Die();
         }
     }
 
+    private IEnumerator TakenDamageVisualise() {
+        spriteRenderer.sprite = damagedSprite;
+        print(Time.time);
+        yield return new WaitForSeconds(0.1f);
+        //spriteRenderer.sprite = defaultSprite;
+        //print(Time.time);
+        yield return null;
+    }
+
     public void Die() {
-        StartCoroutine("SpawnExplosionEnum");
+        invincible = true;
+        StartCoroutine("Death");
+        anim.SetBool("Dead", true);
+    }
+
+    private IEnumerator Death() {
+        yield return null;
     }
 
     private IEnumerator SpawnInNextBoss() {
@@ -36,5 +71,11 @@ public class BossPhaseTwo : MonoBehaviour {
         }
         yield return new WaitForSeconds(6.0f);
         Destroy(gameObject);
+    }
+
+    private IEnumerator Spawn() {
+        yield return new WaitForSeconds(3.0f);
+        anim.SetTrigger("Spawn");
+        invincible = false;
     }
 }
