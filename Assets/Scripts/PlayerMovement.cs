72d0342daed3acddf6487aab52b7ffbfb0b8931a
gustiir <gustiir@gmail.com>
2019-03-20 16:53:45 +0100

    cleaned up character

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,17 +1,16 @@
 ﻿using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour,/*IDamageable*/ {
+
     public CharacterController2D controller;
 
-    public float speed = 80f;
+    [SerializeField] public float speed = 80f;
     float horizontalMove = 0f;
-    bool jump = false;
+
     private Animator anim;
-    private bool isWalking;
-    private bool isJumping;
-    private bool isRunning;
+    public bool jump = false;
     private Rigidbody2D rb;
-    private float dirX;
+
 
     [SerializeField] Projectile projectilePrefab;
     [SerializeField] int damage = 1;
@@ -21,47 +20,76 @@ public class PlayerMovement : MonoBehaviour {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    private void Fire() {
-        if(Input.GetButtonDown("Fire1")) {
+    void Update() {
+        Fire();
+        Move();
+    }
+
+    private void Move()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal");
+        anim.SetFloat("VelocityX", (Mathf.Abs(horizontalMove)));
+        anim.SetFloat("VelocityY", rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump") && rb.velocity.y == 0)
+        {
+            rb.AddForce(Vector2.up * 200f);
+            jump = true;
+        }
+    }
+
+    private void Fire()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            anim.SetTrigger("isShooting");
             Projectile pr = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
             pr.Damage = damage;
             pr.IgnoreCollision(gameObject);
         }
     }
 
-    void Update() {
-        Fire();
-
-        horizontalMove = Input.GetAxisRaw("Horizontal") * speed;
-
-        if(Input.GetButtonDown("Jump") && rb.velocity.y == 0) {
-            rb.AddForce(Vector2.up * 200f);
-            jump = true;
-        }
-
-        if(Mathf.Abs(dirX) > 0 && rb.velocity.y == 0)
-            anim.SetBool("isWalking", true);
-        else
-            anim.SetBool("isWalking", false);
-
-        if(rb.velocity.y > 0)
-            anim.SetBool("isJumping", true);
-        if(rb.velocity.y <= 0)
-            anim.SetBool("isJumping", false);
-
-        if(Mathf.Abs(horizontalMove) == 0) {
-            anim.SetBool("isWalking", false);
-        } else if(Mathf.Abs(horizontalMove) <= 0.4) {
-            anim.SetBool("isRunning", false);
-            anim.SetBool("isWalking", true);
-        } else if(Mathf.Abs(horizontalMove) >= 0.4) {
-            anim.SetBool("isRunning", true);
-            anim.SetBool("isWalking", false);
-        }
-    }
-
     void FixedUpdate() {
-        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        controller.Move(horizontalMove * speed * Time.fixedDeltaTime, false, jump);
         jump = false;
     }
+
+    //public void TakeDamage(int damage = 1)
+    //{
+    //    throw new System.NotImplementedException();
+    //}
+
+    //public void Die()
+    //{
+    //    throw new System.NotImplementedException();
+    //}
 }
+
+
+
+
+//if (rb.velocity.y == 0)
+//    anim.SetBool("isWalking", true);
+//else 
+//    anim.SetBool("isWalking", false);
+
+//if (rb.velocity.y > 0)
+//    anim.SetBool("isJumping", true);
+
+//if (rb.velocity.y <= 0)
+//    anim.SetBool("isJumping", false);
+
+//if (Mathf.Abs(horizontalMove) == 0)
+//{
+//    anim.SetBool("isWalking", false);
+//}
+//else if (Mathf.Abs(horizontalMove) <= 0.4)
+//{
+//    anim.SetBool("isRunning", false);
+//    anim.SetBool("isWalking", true);
+//}
+//else if (Mathf.Abs(horizontalMove) >= 0.4)
+//{
+//    anim.SetBool("isRunning", true);
+//    anim.SetBool("isWalking", false);
+//}
