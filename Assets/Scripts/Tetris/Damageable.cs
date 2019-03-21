ef8c7752dea0e0611413f43544650572fa82f019
TheHampus <Hampus.gogolos@futuregames.nu>
2019-03-21 18:37:28 +0100

    Byter sprite på tetris block
    Byter sprite på tetris block (I-block)

--- Assets/Scripts/Tetris/Damageable.cs
+++ Assets/Scripts/Tetris/Damageable.cs
@@ -1,19 +1,11 @@
 ﻿using UnityEngine;
 
 public class Damageable : MonoBehaviour, IDamageable {
-    [SerializeField]
-    private Thing[] sprites = new Thing[4];
+    public Thing[] sprites = new Thing[4];
 
 
     [SerializeField]
     private int health = 4;
-
-    private void Start() {
-        for(int i = 0; i < 4; ++i) {
-            sprites[i].sprites = new Sprite[4];
-        }
-        UpdateVisuals();
-    }
 
     private void Update() {
         if(Input.GetKeyDown(KeyCode.LeftShift)) {
@@ -35,7 +27,7 @@ public class Damageable : MonoBehaviour, IDamageable {
     }
 
     private void UpdateVisuals() {
-        for(int i = transform.childCount; i == 0; --i) {
+        for(int i = 0; i < transform.childCount; ++i) {
             transform.GetChild(i).GetComponent<SpriteRenderer>().sprite = sprites[health].sprites[i];
         }
     }
