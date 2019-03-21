8d95eda0d78cb7317ab90920545f9cfe2ed16781
gustiir <gustiir@gmail.com>
2019-03-21 16:45:47 +0100

    Added Main and pause menu (Super ugly and no names in credits)

--- Assets/Scripts/UI/Menu.cs
+++ Assets/Scripts/UI/Menu.cs
@@ -4,5 +4,17 @@ using UnityEngine.SceneManagement;
 public class Menu : MonoBehaviour {
     public void Play() {
         SceneManager.LoadScene("Tetris");
+        Time.timeScale = 1f;
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_WEBPLAYER
+         Application.OpenURL(webplayerQuitURL);
+#else
+         Application.Quit();
+#endif
     }
 }
