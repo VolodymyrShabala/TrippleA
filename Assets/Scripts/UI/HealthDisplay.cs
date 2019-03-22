a7f59d5b1cdbedc8ee14354909ff5259f6a2ed54
Volodymyr Shabala <volodymyr.shabala@gmail.com>
2019-03-22 11:22:24 +0100

    Added hp UI and restore hp

---
+++ Assets/Scripts/UI/HealthDisplay.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class HealthDisplay : MonoBehaviour {
+    public void IncreaseHealth() {
+        for(int i = 0; i < transform.childCount; ++i) {
+            if(!transform.GetChild(i).gameObject.activeInHierarchy) {
+                transform.GetChild(i).gameObject.SetActive(true);
+                break;
+            }
+        }
+    }
+
+    public void DecreaseHealth() {
+        for(int i = transform.childCount - 1; i > 0; --i) {
+            if(transform.GetChild(i).gameObject.activeInHierarchy) {
+                transform.GetChild(i).gameObject.SetActive(false);
+                break;
+            }
+        }
+    }
+}
